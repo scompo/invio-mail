70e2ab38ef080fabcd4010e6136bacc1943d7511
Mauro Scomparin <scompo@gmail.com>
2018-03-02 01:57:12 +0100

    Added mail and addresses pages

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var addresses = require('./routes/addresses');
+var mail = require('./routes/mail');
 
 var app = express();
 
@@ -28,7 +29,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/addresses', addresses);
+app.use('/mail', mail);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
